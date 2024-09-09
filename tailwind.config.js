27a92938e7f5941a546581576b7b0d09d2bd1f02
veronica-76sh <veronicasahu007@gmail.com>
2024-09-09 12:59:46 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html',],
+  theme: {  fontFamily: {
+    primary: ['Poppins', 'sans-serif'],
+    },
+  container: {
+    padding: {
+      DEFAULT: '1rem',
+      lg: '2rem',
+    },
+  },
+  screens: {
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1234px',
+  },
+    extend: {
+      colors: {
+        primary: '#101828',
+        secondary: '#7F56D9',
+      },
+      boxShadow: {
+        1: '0px 4px 30px rgba(0, 0, 0, 0.08)',
+      },
+    },
+  },
+  plugins: [],
+};
+
+// module.exports = {
+//     content: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html',
+        
+//     ],
+//     theme: {
+//       fontFamily: {
+//         primary: ['Poppins', 'sans-serif'],
+//       },
+//       container: {
+//         padding: {
+//           DEFAULT: '1rem',
+//           lg: '2rem',
+//         },
+//       },
+//       screens: {
+//         sm: '640px',
+//         md: '768px',
+//         lg: '1024px',
+//         xl: '1234px',
+//       },
+//       extend: {
+//         colors: {
+//           primary: '#101828',
+//           secondary: '#7F56D9',
+//         },
+//         boxShadow: {
+//           1: '0px 4px 30px rgba(0, 0, 0, 0.08)',
+//         },
+//       },
+//     },
+//     plugins: [],
+//   };
+
